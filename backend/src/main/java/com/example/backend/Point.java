437e99fb4b9c69a8943e3727ebb53b989ae28314
muhannadnasr <61321264+muhannadnasr@users.noreply.github.com>
2020-11-24 14:14:32 +0200

    Point class

---
+++ backend/src/main/java/com/example/backend/Point.java
@@ -0,0 +1,30 @@
+package com.example.backend;
+
+public class Point {
+    
+    private double x,y;
+
+    public void setX(double x){
+
+        this.x = x;
+
+    }
+    public double getX(){
+
+        return x;
+
+    }
+    public void setY(double y){
+
+        this.y = y;
+
+    }
+    public double getY(){
+
+        return y;
+        
+    }
+
+
+    
+}
