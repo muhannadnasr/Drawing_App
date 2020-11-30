05bc13c3d7032a4ad26403655a572e29b04eaab6
Ali Hassan <alihassanyussef@gmail.com>
2020-11-30 20:41:24 +0200

    json update

--- backend/src/main/java/com/example/backend/Misc.java
+++ backend/src/main/java/com/example/backend/Misc.java
@@ -6,42 +6,55 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 class square {
     private int length;
     private int width;
+    private Point corner;
     square(){
         this.length = 0;
         this.width = 0;
+        this.corner = null;
     }
-    public int getlength() {
-      return length;
+
+    public int getLength() {
+        return length;
     }
-    public void setlength(int id) {
-      this.length = id;
+
+    public void setLength(int length) {
+        this.length = length;
     }
-    public int getwidth() {
-      return width;
+
+    public int getWidth() {
+        return width;
     }
-    public void setwidth(int name) {
-      this.width = name;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public Point getCorner() {
+        return corner;
+    }
+
+    public void setCorner(Point corner) {
+        this.corner = corner;
     }
 }
 
 class test {
-
     public static String obj2json(Object a) {
         ObjectMapper mapper = new ObjectMapper();
         String jsonStr = "";
         try {
-            jsonStr = mapper.writeValueAsString(a);
+            jsonStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(a);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
         return jsonStr;
-
     }
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
             square sq = new square();
-            sq.setlength(10);
-            sq.setwidth(15);
+            sq.setLength(10);
+            sq.setWidth(15);
+            sq.setCorner(new Point(1, 2));
             String output = obj2json(sq);
             System.out.println(output);
    }
